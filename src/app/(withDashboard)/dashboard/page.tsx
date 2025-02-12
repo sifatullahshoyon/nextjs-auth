b5935c19b99cdff614a31451383ae945c662a194
freelancersifat627e <freelancersifat627@gmail.comm>
2025-02-12 13:15:18 +0600

    Google authentication system implement done

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,8 +1,30 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-    </div>
+    <>
+      {session?.user && (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h4 className="my-5 text-center">
+            Logged-In User Email: {session?.user?.email}
+          </h4>
+          <Image
+            src={session.user.image as string}
+            alt="user img"
+            width="100"
+            height="100"
+            className="rounded-full mx-auto"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
